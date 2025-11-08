b260add9c654edd968197ee425073bf57071524b
sanmesh4090 <sanmesh2004@gmail.com>
2025-11-08 20:27:47 +0530

    deploy

--- dypcet/src/landing_page/home/Awards.js
+++ dypcet/src/landing_page/home/Awards.js
@@ -5,9 +5,9 @@ function Awards() {
         <div className='container mt-5 mb-5'>
             <div className='row '>
                 <div className='col-6 p-5'>
-                        <img src='images/new.jpg'/>
+                        <img src='images/pic.jpg'style={{width :"90%"}}/>
                 </div>
-                    <div className='col-6 p-5 mt-5'>
+                    <div className='col-6 p-5 mt-5 align:center' >
                         <h2> Welcome to Dypcet</h2>
                         <p className='mb-5'>D. Y. Patil College of Engineering & Technology, Kasaba Bawada, Kolhapur is a self-financing Autonomous Institute established in the year 1984 under the visionary leadership of Padmashree Dr. D. Y. Patil, Former Governor of Tripura, Bihar and West Bengal. UGC, New Delhi and Shivaji University, Kolhapur has conferred Autonomous status to the Institute in 2020. The Institution is approved by All India Council for Technical Education (AICTE), New Delhi, Government of Maharashtra and affiliated to Shivaji University, Kolhapur. The Institute is accredited by National Assessment and Accreditation Council (NAAC), Bangalore with ‘A’ grade (CGPA- 3.08). The Institute is offering quality technical education for the past 41 years. The Institute offers eight UG programmes and two PG programmes. Two departments of this Institution are recognized as research centres by Shivaji University, Kolhapur for promoting research culture in the Institute. Architecture department of the Institute is ranked amongst the top 13 Architecture Colleges in India by Outlook India and ranked 21st by India Today in 2021. Three UG programmes viz., B. Tech (Computer Science and Engineering), B. Tech. (Electronics & Telecommunication Engineering) and B. Tech. (Mechanical Engineering) is re-accredited by National Board of Accreditation (NBA), New Delhi for a period of three years from 2022 to 2025</p>
                    <div className='row'>
